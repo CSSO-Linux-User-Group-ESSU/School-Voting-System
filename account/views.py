6afe06d7a31e219a6b30cf8574402e1631a7a7dd
Shirozo <caranzoneilbryan@gmail.com>
2023-10-06 18:50:53 +0800

    Remove register related code

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse
-from .email_backend import EmailBackend
+from .username_backend import UsernameBackend
 from django.contrib import messages
 from .forms import CustomUserForm
 from voting.forms import VoterForm
@@ -16,8 +16,8 @@ def account_login(request):
 
     context = {}
     if request.method == 'POST':
-        user = EmailBackend.authenticate(request, username=request.POST.get(
-            'email'), password=request.POST.get('password'))
+        user = UsernameBackend.authenticate(request, username=request.POST.get(
+            'username'), password=request.POST.get('password'))
         if user != None:
             login(request, user)
             if user.user_type == '1':
@@ -30,27 +30,6 @@ def account_login(request):
 
     return render(request, "voting/login.html", context)
 
-
-def account_register(request):
-    userForm = CustomUserForm(request.POST or None)
-    voterForm = VoterForm(request.POST or None)
-    context = {
-        'form1': userForm,
-        'form2': voterForm
-    }
-    if request.method == 'POST':
-        if userForm.is_valid() and voterForm.is_valid():
-            user = userForm.save(commit=False)
-            voter = voterForm.save(commit=False)
-            voter.admin = user
-            user.save()
-            voter.save()
-            messages.success(request, "Account created. You can login now!")
-            return redirect(reverse('account_login'))
-        else:
-            messages.error(request, "Provided data failed validation")
-            # return account_login(request)
-    return render(request, "voting/reg.html", context)
 
 
 def account_logout(request):

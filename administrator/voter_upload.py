a97278985ccc9ea5a7999e9e78e7fa1e2f883fa9
Shirozo <caranzoneilbryan@gmail.com>
2023-10-09 18:17:24 +0800

    Bug fixes on csv DicReader where header wasn't ignore and is read

--- administrator/voter_upload.py
+++ administrator/voter_upload.py
@@ -7,31 +7,26 @@ from django.contrib.auth.hashers import make_password
 def upload_voters(filepath : str) -> None:
     with open(f'./media/{str(filepath)}', "r") as voter_file:
         try:
-                users = csv.DictReader(voter_file, fieldnames=[
-                                                        "First Name", 
-                                                        "Last Name",
-                                                        "ID Number",
-                                                        "Course",
-                                                        "Year"])
-        except Exception:
-             raise ValueError("Invalid File Type")
+            users = csv.DictReader(voter_file)
+        except Exception as e:
+            raise ValueError(e)
         else:
             for user in users:
                 user_course = Course.objects.get(course=user["Course"])
                 year_level = Voter.YearLevel[user['Year']]
-                uname = f"{str(user['Last name']).lower()}.{str(user['First name']).lower().replace(' ', '')}"
+                uname = f"{str(user['Last Name']).lower()}.{str(user['First Name']).lower().replace(' ', '')}@{str(user['ID Number'])}"
             
                 userObj = CustomUser.objects.create(
-                        password = make_password(user["ID number"]),
-                        first_name = user["First name"],
-                        last_name = user["Last name"],
+                        password = make_password(user["ID Number"]),
+                        first_name = user["First Name"],
+                        last_name = user["Last Name"],
                         username = uname
                         )
                 
                 voter = Voter.objects.create(
                         admin = userObj,
                         course = user_course,
-                        id_number = user["ID number"],
+                        id_number = user["ID Number"],
                         year_level = year_level
                         )
 

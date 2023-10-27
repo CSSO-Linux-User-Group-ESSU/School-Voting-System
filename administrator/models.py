6d090dcaeaa8f5727aad1cba1c8676e7ce41361e
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2023-10-27 15:23:29 +0800

    Added database table for electoral committee

--- administrator/models.py
+++ administrator/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from account.models import CustomUser
+from voting.models import College
 # Create your models here.
+
+class ElectoralCommittee(models.Model):
+    fullname = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    ssc = models.BooleanField(choices=[
+        (True, "Yes"),
+        (False, "No")
+    ])
+    scope = models.ForeignKey(College, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.fullname

7d31b9ac66fc62a4774eb13826a4bb3ef3677a70
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-01-17 09:30:05 +0000

    Fewsss

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -89,18 +89,18 @@ WSGI_APPLICATION = 'e_voting.wsgi.application'
 
 DATABASES = {
     #   You can use this :
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'e_voting',
-        'HOST': '127.0.0.1',
-        'USER': 'root',
-        'PASSWORD': 'essuxlug'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'db_name',
+    #     'HOST': '127.0.0.1',
+    #     'USER': 'db_user',
+    #     'PASSWORD': 'db_password'
+    # }
 }
 
 
